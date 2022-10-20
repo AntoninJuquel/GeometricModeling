7da1806b717480e2caec9ade007e4ded185efba5
Tadres <nicolas.tadres@edu.esiee.fr>
2022-10-20 13:30:35 +0200

    Chips

--- Unity/Assets/Scripts/Chips.cs
+++ Unity/Assets/Scripts/Chips.cs
@@ -10,6 +10,7 @@ public class Chips : MonoBehaviour
     void Start()
     {
         m_Mf = GetComponent<MeshFilter>();
+        m_Mf.mesh = CreateChips(new Vector3(4,2,3));
     }
 
     private Mesh CreateChips(Vector3 halfSize){
@@ -19,6 +20,32 @@ public class Chips : MonoBehaviour
         Vector3[] vertices = new Vector3[8];
         int[] quads = new int[3 * 4];
 
+        vertices[0] = new Vector3(halfSize.x, halfSize.y, halfSize.z);
+        vertices[1] = new Vector3(halfSize.x, -halfSize.y, halfSize.z);
+        vertices[2] = new Vector3(halfSize.x, -halfSize.y, -halfSize.z);
+        vertices[3] = new Vector3(halfSize.x, halfSize.y, -halfSize.z);
+        vertices[4] = new Vector3(-halfSize.x, -halfSize.y, halfSize.z);
+        vertices[5] = new Vector3(-halfSize.x, halfSize.y, halfSize.z);
+        vertices[6] = new Vector3(-halfSize.x, halfSize.y, -halfSize.z);
+        vertices[7] = new Vector3(-halfSize.x, -halfSize.y, -halfSize.z);
+
+        //Face 0
+        quads[0] = 0;
+        quads[1] = 1;
+        quads[2] = 2;
+        quads[3] = 3;
+
+        //Face 1
+        quads[4] = 4;
+        quads[5] = 5;
+        quads[6] = 6;
+        quads[7] = 7;
+
+        //Face 2
+        quads[8] = 4;
+        quads[9] = 1;
+        quads[10] = 0;
+        quads[11] = 5;
 
 
         mesh.vertices = vertices;
